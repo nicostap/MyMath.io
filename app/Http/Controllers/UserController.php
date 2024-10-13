93a6c62ee690806d8615395edccf3796631648a6
847980 <137378009+847980@users.noreply.github.com>
2024-10-13 07:46:16 +0700

    leaderboard fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,10 +14,42 @@ class UserController extends Controller
 
     public function leaderboard(User $user)
     {
-        $players = User::where('active', 1)
+        $ranks = ["Beginner", "Novice", "Advance", "Expert", "Legendary"];
+        $beginners = User::where('active', 1)
+            ->where('rating', '<=', 1499)
             ->orderBy('rating', 'desc')
             ->limit(100)
             ->get(['id', 'name', 'rating']);
-        return view('user.leaderboard', ['players' => $players, 'user' => $user]);
+        $novices = User::where('active', 1)
+            ->where('rating', '>=', 1500)
+            ->where('rating', '<=', 2499)
+            ->orderBy('rating', 'desc')
+            ->limit(100)
+            ->get(['id', 'name', 'rating']);
+        $advances = User::where('active', 1)
+            ->where('rating', '>=', 2500)
+            ->where('rating', '<=', 3999)
+            ->orderBy('rating', 'desc')
+            ->limit(100)
+            ->get(['id', 'name', 'rating']);
+        $experts = User::where('active', 1)
+            ->where('rating', '>=', 4000)
+            ->where('rating', '<=', 6499)
+            ->orderBy('rating', 'desc')
+            ->limit(100)
+            ->get(['id', 'name', 'rating']);
+        $legendaries = User::where('active', 1)
+            ->where('rating', '>=', 6500)
+            ->orderBy('rating', 'desc')
+            ->limit(100)
+            ->get(['id', 'name', 'rating']);
+        $data = [
+            $ranks[0] => $beginners, 
+            $ranks[1] => $novices, 
+            $ranks[2] => $advances, 
+            $ranks[3] => $experts, 
+            $ranks[4] => $legendaries
+        ];
+        return view('user.leaderboard', ["ranks" => $data, 'user' => $user]);
     }
 }
